d0630576effc92d0ecffd22eda5cf82ae7e1221b
Maarten ter Huurne <maarten@treewalker.org>
2011-04-13 03:49:57 +0200

    gmenu2x: Added package
    Application menu for devices with a 320x240 screen and buttons.

---
+++ package/gmenu2x/gmenu2x.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# gmenu2x
+#
+################################################################################
+GMENU2X_VERSION = master
+GMENU2X_SITE = git://projects.qi-hardware.com/gmenu2x.git
+GMENU2X_DEPENDENCIES = dejavu libpng sdl sdl_ttf
+GMENU2X_AUTORECONF = YES
+GMENU2X_CONF_OPT = \
+	--with-sdl-prefix=$(STAGING_DIR)/usr \
+	--enable-platform=$(BR2_PACKAGE_GMENU2X_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_LIBOPK),y)
+GMENU2X_DEPENDENCIES += libopk
+endif
+
+ifeq ($(BR2_PACKAGE_LIBXDGMIME),y)
+GMENU2X_DEPENDENCIES += libxdgmime
+endif
+
+$(eval $(autotools-package))
