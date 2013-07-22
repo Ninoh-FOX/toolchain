03df18b13210e9789f91617420a07e75b6a2892d
Maarten ter Huurne <maarten@treewalker.org>
2013-07-22 19:50:00 +0200

    shared-mime-info: Declare unconditional dependency on gettext
    Creating the configure script fails if gettext hasn't been installed.
    
    For some reason, even host-shared-mime-info depends on gettext, which
    might indicate that host and target files are being mixed up here.

--- package/shared-mime-info/shared-mime-info.mk
+++ package/shared-mime-info/shared-mime-info.mk
@@ -9,13 +9,16 @@ SHARED_MIME_INFO_SOURCE = shared-mime-info-$(SHARED_MIME_INFO_VERSION).tar.bz2
 SHARED_MIME_INFO_SITE = http://freedesktop.org/~hadess
 SHARED_MIME_INFO_INSTALL_STAGING = YES
 SHARED_MIME_INFO_CONF_ENV = XMLLINT=$(HOST_DIR)/usr/bin/xmllint
-SHARED_MIME_INFO_DEPENDENCIES = host-pkgconf host-libglib2 host-libxml2 host-shared-mime-info libxml2 libglib2
+SHARED_MIME_INFO_DEPENDENCIES = host-pkgconf host-intltool host-libglib2 \
+		host-libxml2 host-shared-mime-info libxml2 libglib2 \
+		$(if $(BR2_NEEDS_GETTEXT),gettext)
 SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
 SHARED_MIME_INFO_MAKE = $(MAKE1)
 SHARED_MIME_INFO_LICENSE = GPLv2
 SHARED_MIME_INFO_LICENSE_FILES = COPYING
 
-HOST_SHARED_MIME_INFO_DEPENDENCIES = host-pkgconf host-intltool
+HOST_SHARED_MIME_INFO_DEPENDENCIES = host-pkgconf host-intltool \
+		$(if $(BR2_NEEDS_GETTEXT),gettext)
 
 HOST_SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
 HOST_SHARED_MIME_INFO_MAKE = $(MAKE1)
