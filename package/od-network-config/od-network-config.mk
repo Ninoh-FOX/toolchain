846f9943f7cf23270ec9d7f1bd9f15b013da1efd
Maarten ter Huurne <maarten@treewalker.org>
2013-09-12 19:41:27 +0200

    od-network-config: Added package
    This allows the user to set a password for logging in. It requires the
    use of a shadow password database.
    
    The implementation is in this repo. If it grows larger we might have to
    create a dedicated repo for it.
    
    The code was written by me, the icon was drawn by hi-ban.

---
+++ package/od-network-config/od-network-config.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# od-network-config
+#
+#############################################################
+
+OD_NETWORK_CONFIG_SITE = package/od-network-config
+OD_NETWORK_CONFIG_SITE_METHOD = local
+
+OD_NETWORK_CONFIG_DEPENDENCIES = python-pygame
+
+ifeq ($(BR2_PACKAGE_GMENU2X),y)
+OD_NETWORK_CONFIG_DEPENDENCIES += gmenu2x
+define OD_NETWORK_CONFIG_INSTALL_TARGET_GMENU2X
+	$(INSTALL) -m 0644 -D $(@D)/gmenu2x $(TARGET_DIR)/usr/share/gmenu2x/sections/settings/60_network
+	$(INSTALL) -m 0644 -D $(@D)/icon.png $(TARGET_DIR)/usr/share/gmenu2x/skins/Default/icons/network.png
+endef
+endif
+
+define OD_NETWORK_CONFIG_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/od-network-config.py $(TARGET_DIR)/usr/bin/od-network-config
+	$(OD_NETWORK_CONFIG_INSTALL_TARGET_GMENU2X)
+endef
+
+$(eval $(generic-package))
