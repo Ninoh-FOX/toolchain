774a6b47dda487a36d3c25408738a23f97871758
Paul Cercueil <paul@crapouillou.net>
2013-11-29 02:16:35 +0100

    opendingux: GCW0: Add a post-build cleanup script

---
+++ board/opendingux/gcw0/post-build-script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+TARGET_DIR=$1
+
+# Remove binutils executables we don't use
+for i in ar as ld ld.bfd nm objcopy ranlib strip ; do
+	rm -f ${TARGET_DIR}/usr/mipsel-gcw0-linux-uclibc/bin/$i
+done
+
+# We use xinetd, so no need for a startup script for the SSH daemons
+# and the FTP server.
+rm -f ${TARGET_DIR}/etc/init.d/S50sshd
+rm -f ${TARGET_DIR}/etc/init.d/S50dropbear
+rm -f ${TARGET_DIR}/etc/init.d/S70vsftpd
+
+# The NTP daemon is started in the /etc/network/if-up.d/ntpd
+# script and stopped in /etc/network/if-post-down.d/ntpd
+rm -f ${TARGET_DIR}/etc/init.d/S49ntp
