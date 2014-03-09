6e688001938f9963f79b191d19458176428aa396
Paul Cercueil <paul@crapouillou.net>
2014-03-09 11:57:45 +0100

    gcw-connect: Added package

---
+++ package/gcw-connect/gcw-connect.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# gcw-connect
+#
+################################################################################
+
+GCW_CONNECT_VERSION = master
+GCW_CONNECT_SITE = $(call github,gcwnow,gcwconnect,$(GCW_CONNECT_VERSION))
+GCW_CONNECT_DEPENDENCIES = python python-pygame
+
+ifeq ($(BR2_PACKAGE_GMENU2X),y)
+GCW_CONNECT_DEPENDENCIES += gmenu2x
+define GCW_CONNECT_INSTALL_TARGET_GMENU2X
+	$(INSTALL) -m 0644 -D package/gcw-connect/gmenu2x \
+		$(TARGET_DIR)/usr/share/gmenu2x/sections/settings/60_wireless
+	$(INSTALL) -m 0644 -D $(@D)/icon.png \
+		$(TARGET_DIR)/usr/share/gmenu2x/skins/Default/icons/wireless.png
+endef
+GCW_CONNECT_POST_INSTALL_TARGET_HOOKS += GCW_CONNECT_INSTALL_TARGET_GMENU2X
+endif
+
+define GCW_CONNECT_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/gcwconnect.py \
+		$(TARGET_DIR)/usr/bin/gcwconnect
+	cp -r $(@D)/data $(TARGET_DIR)/usr/share/gcwconnect
+endef
+
+$(eval $(generic-package))
