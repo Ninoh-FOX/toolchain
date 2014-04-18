a7dd8e54b370feffbac3433a52d1ec083c67cdbd
Paul Cercueil <paul@crapouillou.net>
2014-04-18 14:11:51 +0200

    opendingux: Remove all binutils except for objdump, and DRI drivers

--- board/opendingux/gcw0/post-build-script.sh
+++ board/opendingux/gcw0/post-build-script.sh
@@ -4,8 +4,9 @@ TARGET_DIR=$1
 
 # Remove binutils executables we don't use
 for i in ar as ld ld.bfd nm objcopy ranlib strip ; do
-	rm -f ${TARGET_DIR}/usr/mipsel-gcw0-linux-uclibc/bin/$i
+	rm -f ${TARGET_DIR}/usr/bin/$i
 done
+rm -rf ${TARGET_DIR}/usr/mipsel-gcw0-linux-uclibc
 
 # We use xinetd, so no need for a startup script for the SSH daemons
 # and the FTP server.
@@ -16,3 +17,6 @@ rm -f ${TARGET_DIR}/etc/init.d/S70vsftpd
 # The NTP daemon is started in the /etc/network/if-up.d/ntpd
 # script and stopped in /etc/network/if-post-down.d/ntpd
 rm -f ${TARGET_DIR}/etc/init.d/S49ntp
+
+# Remove the DRI drivers
+rm -rf ${TARGET_DIR}/usr/lib/dri
