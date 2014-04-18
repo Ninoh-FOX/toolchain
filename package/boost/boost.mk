6d0e41075b6d60c8867c13b4daf27b9196319dd1
Paul Cercueil <paul@crapouillou.net>
2014-04-18 13:40:55 +0200

    boost: Added a config option to build statically

--- package/boost/boost.mk
+++ package/boost/boost.mk
@@ -83,8 +83,8 @@ HOST_BOOST_OPT += toolset=gcc threading=multi variant=release link=shared \
 BOOST_OPT += toolset=gcc \
 	     threading=multi \
 	     variant=$(if $(BR2_ENABLE_DEBUG),debug,release) \
-	     link=$(if $(BR2_PREFER_STATIC_LIB),static,shared) \
-	     runtime-link=$(if $(BR2_PREFER_STATIC_LIB),static,shared)
+	     link=$(if $(BR2_PACKAGE_BOOST_STATIC),static,shared) \
+	     runtime-link=$(if $(BR2_PACKAGE_BOOST_STATIC),static,shared)
 
 ifeq ($(BR2_PACKAGE_BOOST_LOCALE),y)
 ifeq ($(BR2_TOOLCHAIN_USES_UCLIBC),y)
