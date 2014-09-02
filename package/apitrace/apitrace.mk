b1715da02efae655aafe371541a56107a5cc7546
Maarten ter Huurne <maarten@treewalker.org>
2014-09-03 01:58:16 +0200

    apitrace: Merged gcw0 specific changes
    In the opendingux-2014.05 branch, we replaced the entire apitrace
    package. Instead of doing that, I now apply our changes on top of
    the buildroot package. It should be unraveled further though.
    
    Patches by Wladimir J. van der Laan <laanwj@gmail.com>.

--- package/apitrace/apitrace.mk
+++ package/apitrace/apitrace.mk
@@ -4,14 +4,21 @@
 #
 ################################################################################
 
-APITRACE_VERSION = c181b7fbf4b1c3912424761fec8ac7124640543a
+APITRACE_VERSION = 5.0
 APITRACE_SITE = $(call github,apitrace,apitrace,$(APITRACE_VERSION))
 APITRACE_LICENSE = MIT
 APITRACE_LICENSE_FILES = LICENSE
 
-APITRACE_DEPENDENCIES = xlib_libX11 host-python
+APITRACE_DEPENDENCIES = popt binutils host-python
 
 # Gui was never tested, so we prefer to explicitly disable it
 APITRACE_CONF_OPT += -DENABLE_GUI=false
+
+APITRACE_CONF_OPT = -DPYTHON_EXECUTABLE:STRING="$(HOST_DIR)/usr/bin/python"
+
+ifneq ($(BR2_PACKAGE_XLIB_LIBX11),y)
+APITRACE_CONF_OPT += -DCMAKE_C_FLAGS:STRING="$(TARGET_CFLAGS) -DMESA_EGL_NO_X11_HEADERS"
+APITRACE_CONF_OPT += -DCMAKE_CXX_FLAGS:STRING="$(TARGET_CFLAGS) -DMESA_EGL_NO_X11_HEADERS"
+endif
 
 $(eval $(cmake-package))
