26c34f97071d87781696e2eb51d4091b1d5c9259
Soar Qin <soarchin@gmail.com>
2019-10-19 16:17:50 +0800

    download/build error fixes

--- package/bzip2/bzip2.mk
+++ package/bzip2/bzip2.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 BZIP2_VERSION = 1.0.6
-BZIP2_SITE = http://www.bzip.org/$(BZIP2_VERSION)
+BZIP2_SITE = https://sourceware.org/pub/bzip2
 BZIP2_INSTALL_STAGING = YES
 BZIP2_LICENSE = bzip2 license
 BZIP2_LICENSE_FILES = LICENSE
