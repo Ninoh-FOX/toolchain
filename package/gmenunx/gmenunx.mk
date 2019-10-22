cebaedf4d21f9717e6e9637f0b679be6ecf945d0
podulator <github-account@podulator.com>
2019-10-22 16:43:45 +0200

    bumped gmenunx reference

--- package/gmenunx/gmenunx.mk
+++ package/gmenunx/gmenunx.mk
@@ -3,7 +3,7 @@
 # gmenunx
 #
 ################################################################################
-GMENUNX_VERSION = 72b7b039b
+GMENUNX_VERSION = 0ede1c0a
 GMENUNX_SITE = $(call github,podulator,gmenunx,$(GMENUNX_VERSION))
 GMENUNX_DEPENDENCIES = dejavu libpng sdl sdl_ttf sdl_gfx
 GMENUNX_INSTALL_TARGET = YES
