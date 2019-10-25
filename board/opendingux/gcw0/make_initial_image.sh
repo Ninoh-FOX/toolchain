8f5e2222b0157777807954bec3e67fc50e367baf
Justin DeFields <justindefields@gmail.com>
2019-10-25 08:31:47 -0400

    initial image: updates.  Now pulls apps/emulators from Jutley's site

--- board/opendingux/gcw0/make_initial_image.sh
+++ board/opendingux/gcw0/make_initial_image.sh
@@ -21,12 +21,25 @@ rm -rf output/images/hibernation.ext4
 rm -rf output/images/sdcard.img
 
 # local_pack_root is where the factory version emulators and ports should reside
-mkdir -p output/images/local_pack_root
+mkdir -p output/images/local_pack_root/apps
+
+echo Downloading rs97.bitgala.xyz/RG-350/localpack/emulators/...
+( cd  output/images/local_pack_root/apps ; \
+wget -N -r -nd --no-parent --reject="index.html*" --reject="scumm*" \
+https://rs97.bitgala.xyz/RG-350/localpack/emulators/ ) &> /tmp/emulators.log
+
+echo Downloading rs97.bitgala.xyz/RG-350/localpack/apps/...
+( cd  output/images/local_pack_root/apps ; \
+wget -N -r -nd --no-parent --reject="index.html*" \
+https://rs97.bitgala.xyz/RG-350/localpack/apps/ ) &> /tmp/apps.log
 
 # hiberation_root is just an empty directory to trick genimage into creating
 # an empty ext4 partition.  Always clear it out, as it should be empty
 mkdir -p /tmp/hiberation_root
 rm -rf /tmp/hiberation_root/*
+
+# Clear output/images-tmp
+rm -rf output/images-tmp
 
 # Build sdcard.img from board/opendingux/gcw0/genimage.cfg
 output/host/usr/bin/genimage \
@@ -35,5 +48,8 @@ output/host/usr/bin/genimage \
 	    --inputpath output/images \
 	    --outputpath output/images \
 	    --tmppath output/images-tmp
+
+# Clear output/images-tmp
+rm -rf output/images-tmp
 
 
