6ad33c77a5784ddf4a406cb9e45241c1dfb99d3c
podulator <github-account@podulator.com>
2019-11-06 23:11:15 +0100

    better link mode options

--- package/gmenunx/gmenunx.mk
+++ package/gmenunx/gmenunx.mk
@@ -3,7 +3,7 @@
 # gmenunx
 #
 ################################################################################
-GMENUNX_VERSION = e1e6043c1b3
+GMENUNX_VERSION = 358e0e8de916f
 GMENUNX_SITE = $(call github,podulator,gmenunx,$(GMENUNX_VERSION))
 GMENUNX_DEPENDENCIES = dejavu libpng sdl sdl_ttf sdl_gfx
 GMENUNX_INSTALL_TARGET = YES
