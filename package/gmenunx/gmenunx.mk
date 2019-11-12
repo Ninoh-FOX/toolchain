090bf87aed83b6e42b4492ad95f6c158ff7bdab7
Mat Rowlands <matrow@amazon.de>
2019-11-12 23:23:36 +0100

    nx : many updates

--- package/gmenunx/gmenunx.mk
+++ package/gmenunx/gmenunx.mk
@@ -3,7 +3,7 @@
 # gmenunx
 #
 ################################################################################
-GMENUNX_VERSION = 79216de362ece2
+GMENUNX_VERSION = dd41151cb7f7e
 GMENUNX_SITE = $(call github,podulator,gmenunx,$(GMENUNX_VERSION))
 GMENUNX_DEPENDENCIES = dejavu libpng sdl sdl_ttf sdl_gfx
 GMENUNX_INSTALL_TARGET = YES
