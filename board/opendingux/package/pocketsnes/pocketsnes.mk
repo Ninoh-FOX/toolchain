529b66df3cce95c80d7b3d5f3718b4ffe3c98794
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-12-14 07:46:10 +0000

    PocketSNES: Switch to @soarqin's fork (#39)
    It has some updates from upstream and menu on the power button.

--- board/opendingux/package/pocketsnes/pocketsnes.mk
+++ board/opendingux/package/pocketsnes/pocketsnes.mk
@@ -1,25 +1,23 @@
 ################################################################################
 #
-# pcsx4all
+# PocketSNES
 #
 ################################################################################
 POCKETSNES_VERSION = master
-POCKETSNES_SITE = $(call github,pingflood,PocketSNES,$(POCKETSNES_VERSION))
+POCKETSNES_SITE = $(call github,soarqin,PocketSNES,$(POCKETSNES_VERSION))
 POCKETSNES_AUTORECONF = YES
 POCKETSNES_INSTALL_STAGING = YES
 
 CROSS_COMPILE=$(TARGET_CROSS)
 
 define POCKETSNES_BUILD_CMDS
-	cp -f $(BR2_EXTERNAL)/package/pocketsnes/make_opk.sh $(@D)
     $(MAKE) CROSS_COMPILE="$(TARGET_CROSS)" -C $(@D) all
 endef
 
 define POCKETSNES_INSTALL_OPK_IMAGES
-    (   cd $(@D) ; \
-        ./make_opk.sh ; \
-        mkdir -p $(BINARIES_DIR)/opks ; \
-        cp -vf PocketSNES.opk $(BINARIES_DIR)/opks )
+    cd $(@D) && opk/make_opk.sh
+    mkdir -p $(BINARIES_DIR)/opks
+    cp -f $(@D)/opk/PocketSNES.opk $(BINARIES_DIR)/opks
 endef
 POCKETSNES_POST_INSTALL_TARGET_HOOKS += POCKETSNES_INSTALL_OPK_IMAGES
 
