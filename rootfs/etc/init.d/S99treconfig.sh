922d8fbe4444d4f8bbeb08034341f070d9311169
Ninoh-FOX <ninohfox@gmail.com>
2020-01-03 15:02:20 +0100

    actualizado scripts de arranque

--- rootfs/etc/init.d/S99treconfig.sh
+++ rootfs/etc/init.d/S99treconfig.sh
@@ -3,7 +3,7 @@
 if [ -f /media/data/.reconfig ]; then
    exit 1
  else
-   rm -rf /media/data/local/etc/*
+   rm -rf /media/data/local/etc/dropbear_*
    echo -n 1 >/sys/devices/virtual/vtconsole/vtcon1/bind
    clear
    echo "wipe old config from file system"
