04dd417a8b82d127774adf1aacec14c9ee3e35eb
Ninoh-FOX <ninohfox@gmail.com>
2020-01-04 12:41:03 +0100

    actualizado apitrace 5 a 9

--- package/apitrace/apitrace.mk
+++ package/apitrace/apitrace.mk
@@ -4,21 +4,21 @@
 #
 ################################################################################
 
-APITRACE_VERSION = 5.0
+APITRACE_VERSION = 9.0
 APITRACE_SITE = $(call github,apitrace,apitrace,$(APITRACE_VERSION))
 APITRACE_LICENSE = MIT
 APITRACE_LICENSE_FILES = LICENSE
 
-APITRACE_DEPENDENCIES = popt binutils host-python
+APITRACE_DEPENDENCIES = host-python libpng
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+APITRACE_DEPENDENCIES += xlib_libX11
+APITRACE_CONF_OPT += -DENABLE_X11=ON
+else
+APITRACE_CONF_OPT += -DENABLE_X11=OFF
+endif
 
 # Gui was never tested, so we prefer to explicitly disable it
 APITRACE_CONF_OPT += -DENABLE_GUI=false
-
-APITRACE_CONF_OPT = -DPYTHON_EXECUTABLE:STRING="$(HOST_DIR)/usr/bin/python"
-
-ifneq ($(BR2_PACKAGE_XLIB_LIBX11),y)
-APITRACE_CONF_OPT += -DCMAKE_C_FLAGS:STRING="$(TARGET_CFLAGS) -DMESA_EGL_NO_X11_HEADERS"
-APITRACE_CONF_OPT += -DCMAKE_CXX_FLAGS:STRING="$(TARGET_CFLAGS) -DMESA_EGL_NO_X11_HEADERS"
-endif
 
 $(eval $(cmake-package))
