9ce4deec24a914e9177c87d5e30417032fc9e262
Ninoh-FOX <ninohfox@gmail.com>
2020-01-05 01:36:54 +0100

    update sdl2_ttf

--- package/sdl2_ttf/sdl2_ttf.mk
+++ package/sdl2_ttf/sdl2_ttf.mk
@@ -4,19 +4,32 @@
 #
 ################################################################################
 
-SDL2_TTF_VERSION = 2.0.12
+SDL2_TTF_VERSION = 2.0.14
 SDL2_TTF_SOURCE = SDL2_ttf-$(SDL2_TTF_VERSION).tar.gz
 SDL2_TTF_SITE = http://www.libsdl.org/projects/SDL_ttf/release/
 SDL2_TTF_INSTALL_STAGING = YES
 SDL2_TTF_LICENSE = zlib
 SDL2_TTF_LICENSE_FILES = COPYING.txt
+SDL2_TTF_INSTALL_STAGING = YES
+SDL2_TTF_DEPENDENCIES = sdl2 freetype host-pkgconf
 
-SDL2_TTF_DEPENDENCIES = sdl2 freetype
+# Two patches touching configure.in
+SDL2_TTF_AUTORECONF = YES
 
-SDL2_TTF_CONF_OPT = --without-x \
-		--with-freetype-prefix=$(STAGING_DIR)/usr \
-		--with-sdl-prefix=$(STAGING_DIR)/usr
+SDL2_TTF_CONF_ENV = \
+	FREETYPE_CONFIG=$(STAGING_DIR)/usr/bin/freetype-config
 
-#SDL2_TTF_MAKE_OPT = INCLUDES="-I$(STAGING_DIR)/usr/include/SDL2"  LDFLAGS="-L$(STAGING_DIR)/usr/lib"
+# x-includes and x-libraries must be set for cross-compiling
+# By default x_includes and x_libraries contains unsafe paths.
+# (/usr/include and /usr/lib)
+ifeq ($(BR2_PACKAGE_SDL2_X11),y)
+SDL2_TTF_CONF_OPT += \
+	--with-x \
+	--x-includes=$(STAGING_DIR)/usr/include \
+	--x-libraries=$(STAGING_DIR)/usr/lib
+else
+SDL2_TTF_CONF_OPT += \
+	--without-x
+endif
 
 $(eval $(autotools-package))
