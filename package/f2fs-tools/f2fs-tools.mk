1522c1d79bbd570c30307176154e569458de1a24
Ninoh-FOX <ninohfox@gmail.com>
2020-01-21 18:12:32 +0100

    Update f2fs_tools

--- package/f2fs-tools/f2fs-tools.mk
+++ package/f2fs-tools/f2fs-tools.mk
@@ -1,13 +1,13 @@
-
 ################################################################################
 #
 # f2fs-tools
 #
 ################################################################################
 
-F2FS_TOOLS_VERSION = 1.11.0
-F2FS_TOOLS_SOURCE = f2fs-tools_$(F2FS_TOOLS_VERSION).orig.tar.gz
-F2FS_TOOLS_SITE = http://ftp.debian.org/debian/pool/main/f/f2fs-tools/
+F2FS_TOOLS_VERSION = 858c4039c85f7c00c57925902985701b1e083555
+F2FS_TOOLS_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs-tools.git
+F2FS_TOOLS_SITE_METHOD = git
+F2FS_TOOLS_CONF_ENV = ac_cv_file__git=no
 F2FS_TOOLS_DEPENDENCIES = host-pkgconf util-linux
 # GIT version, shipped without configure
 F2FS_TOOLS_AUTORECONF = YES
