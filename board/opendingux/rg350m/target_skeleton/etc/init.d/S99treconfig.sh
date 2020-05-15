af3a431c2c38b3f35144cf537771fd3c88bd9c7f
ninoh-fox <ninoh-fox@debian>
2020-05-15 13:22:18 +0200

    added RG350M board

---
+++ board/opendingux/rg350m/target_skeleton/etc/init.d/S99treconfig.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -f /media/data/.reconfig ]; then
+   exit 1
+ else
+   rm -rf /media/data/local/etc/*
+   echo -n 1 >/sys/devices/virtual/vtconsole/vtcon1/bind
+   clear
+   echo "wipe old config from file system"
+   sleep 4
+   echo "done"
+   sleep 2
+   echo "The system reboot in 10s..."
+   sleep 10
+   echo -n 0 >/sys/devices/virtual/vtconsole/vtcon1/bind
+   touch /media/data/.reconfig
+   reboot
+ fi
