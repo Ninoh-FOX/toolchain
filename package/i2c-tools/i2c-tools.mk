8cd8ef17b52f6c5a87d1d5d01dc2d8b25fb4be22
ninoh-fox <ninoh-fox@debian>
2020-05-22 19:02:46 +0200

    changed sources

--- package/i2c-tools/i2c-tools.mk
+++ package/i2c-tools/i2c-tools.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 I2C_TOOLS_VERSION = 4.1
-I2C_TOOLS_SOURCE = i2c-tools-$(I2C_TOOLS_VERSION).tar.xz
-I2C_TOOLS_SITE = http://sources.buildroot.net/i2c-tools/
+I2C_TOOLS_SOURCE = i2c-tools_$(I2C_TOOLS_VERSION).orig.tar.xz
+I2C_TOOLS_SITE = http://deb.debian.org/debian/pool/main/i/i2c-tools
 I2C_TOOLS_LICENSE = GPLv2+, GPLv2 (py-smbus)
 I2C_TOOLS_LICENSE_FILES = COPYING
 
