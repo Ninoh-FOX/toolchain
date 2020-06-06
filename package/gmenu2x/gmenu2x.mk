8fa1ea65413a250d330776f2dde33b21f6dabb97
ninoh-fox <ninoh-fox@debian>
2020-06-06 13:12:02 +0200

    fix type

--- package/gmenu2x/gmenu2x.mk
+++ package/gmenu2x/gmenu2x.mk
@@ -8,7 +8,6 @@ GMENU2X_SITE = $(call github,Ninoh-FOX,gmenu2x,$(GMENU2X_VERSION))
 GMENU2X_DEPENDENCIES = dejavu libpng sdl sdl_ttf
 GMENU2X_AUTORECONF = YES
 GMENU2X_CONF_OPT = \
-	-DBIND_CONSOLE=ON \
 	--with-sdl-prefix=$(STAGING_DIR)/usr \
 	--enable-platform=$(BR2_PACKAGE_GMENU2X_PLATFORM) \
 	--enable-stick=$(BR2_PACKAGE_GMENU2X_STICK) \
