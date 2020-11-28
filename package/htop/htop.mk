888c3bb60a5ce896e5d0685c59aa2e9639cf1aea
Ninoh-FOX <ninohfox@gmail.com>
2020-11-28 11:59:57 +0100

    fix htop package source

--- package/htop/htop.mk
+++ package/htop/htop.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HTOP_VERSION = 2.2.0
-HTOP_SITE = http://hisham.hm/htop/releases/$(HTOP_VERSION)
+HTOP_SITE = https://github.com/hishamhm/htop/releases/$(HTOP_VERSION)
 HTOP_DEPENDENCIES = ncurses
 # Prevent htop build system from searching the host paths
 HTOP_CONF_ENV = HTOP_NCURSES_CONFIG_SCRIPT=$(STAGING_DIR)/usr/bin/$(NCURSES_CONFIG_SCRIPTS)
