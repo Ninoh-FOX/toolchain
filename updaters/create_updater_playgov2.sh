ea2797558cf81d915407a4b2541ef3e65c3e7872
ninoh-fox <ninoh-fox@debian>
2021-01-07 13:02:41 +0100

    add playgo v2 device

---
+++ updaters/create_updater_playgov2.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+set -e
+umask 0022
+
+VERSION="`cat ../version.txt`"
+
+if [ -r "../output/images/vmlinuz.bin" ]; then
+KERNEL="../output/images/vmlinuz.bin"
+else
+KERNEL=""
+fi
+
+if [ -r "../output/images/modules.squashfs" ]; then
+MODULES_FS="../output/images/modules.squashfs"
+else
+MODULE_FS=""
+fi
+
+if [ -r "../output/images/rootfs.squashfs" ]; then
+ROOTFS="../output/images/rootfs.squashfs"
+else
+ROOTFS=""
+fi
+
+if [ -r "updateplayv2/mininit-syspart" ]; then
+MININIT="updateplayv2/mininit-syspart"
+else
+MININIT=""
+fi
+
+if [ -r "updateplayv2/ubiboot.bin" ]; then
+BOOTLOADERS="updateplayv2/ubiboot.bin"
+else
+BOOTLOADERS=""
+fi
+
+# TODO: Reinstate this:
+# X-OD-Manual=CHANGELOG
+
+# Copy kernel and rootfs to update dir.
+# We want to support symlinks for the kernel and rootfs images and if no
+# copy is made, specifying the symlink will include the symlink in the OPK
+# and specifying the real path might use a different name than the updatev2
+# script expects.
+if [ "$KERNEL" -a "$ROOTFS" ] ; then
+	if [ `date -r "$KERNEL" +%s` -gt `date -r "$ROOTFS" +%s` ] ; then
+		DATE=`date -r "$KERNEL" +%F`
+	else
+		DATE=`date -r "$ROOTFS" +%F`
+	fi
+
+elif [ "$KERNEL" ] ; then
+	DATE=`date -r "$KERNEL" +%F`
+elif [ "$ROOTFS" ] ; then
+	DATE=`date -r "$ROOTFS" +%F`
+else
+	echo "ERROR: No kernel or rootfs found."
+	exit 1
+fi
+
+if [ "$KERNEL" ] ; then
+
+	chmod a-x "$KERNEL" "$MODULES_FS"
+
+	echo -n "Calculating SHA1 sum of kernel... "
+	sha1sum "$KERNEL" | cut -d' ' -f1 > "../output/images/vmlinuz.bin.sha1"
+	echo "done"
+
+	echo -n "Calculating SHA1 sum of modules file-system... "
+	sha1sum "$MODULES_FS" | cut -d' ' -f1 > "../output/images/modules.squashfs.sha1"
+	echo "done"
+
+	KERNEL="$KERNEL ../output/images/vmlinuz.bin.sha1"
+        MODULES_FS="$MODULES_FS ../output/images/modules.squashfs.sha1"
+fi
+
+if [ "$ROOTFS" ] ; then
+
+	echo -n "Calculating SHA1 sum of rootfs... "
+	sha1sum "$ROOTFS" | cut -d' ' -f1 > "../output/images/rootfs.squashfs.sha1"
+	echo "done"
+
+	ROOTFS="$ROOTFS ../output/images/rootfs.squashfs.sha1"
+fi
+
+if [ "$BOOTLOADERS" ] ; then
+
+	echo -n "Calculating SHA1 sum of bootloaders... "
+        sha1sum "$BOOTLOADERS" | cut -d' ' -f1 > "updateplayv2/ubiboot.bin.sha1"
+        echo "done"
+
+        BOOTLOADERS="$BOOTLOADERS updateplayv2/ubiboot.bin.sha1"
+fi
+
+if [ "$MININIT" ] ; then
+
+	echo -n "Calculating SHA1 sum of mininit-syspart... "
+	sha1sum "$MININIT" | cut -d' ' -f1 > "updateplayv2/mininit-syspart.sha1"
+	echo "done"
+
+	MININIT="$MININIT updateplayv2/mininit-syspart.sha1"
+fi
+
+echo "$DATE" > updateplayv2/date.txt
+
+echo "$VERSION" > updateplayv2/version.txt
+
+# Report metadata.
+echo
+echo "=========================="
+echo "Bootloaders:          $BOOTLOADERS"
+echo "Mininit:              $MININIT"
+echo "Kernel:               $KERNEL"
+echo "Modules file system:  $MODULES_FS"
+echo "Root file system:     $ROOTFS"
+echo "build date:           $DATE"
+echo "build version:        $VERSION"
+echo "=========================="
+echo
+
+# Write metadata.
+cat > updateplayv2/default.gcw0.desktop <<EOF
+[Desktop Entry]
+Name=OS update $VERSION
+Comment=PLAYGO v2 ROGUE update $DATE
+Exec=update.sh
+Icon=rogue
+Terminal=true
+Type=Application
+StartupNotify=true
+Categories=applications;
+EOF
+
+# Create OPK.
+OPK_FILE=updateplayv2/playgov2-update-$VERSION-$DATE.opk
+mksquashfs \
+	updateplayv2/default.gcw0.desktop \
+	updateplayv2/rogue.png \
+	updateplayv2/update.sh \
+	updateplayv2/trimfat.py \
+	updateplayv2/flash_partition.sh \
+	updateplayv2/date.txt \
+    updateplayv2/version.txt \
+    updateplayv2/fsck.fat \
+	$BOOTLOADERS \
+	$MININIT \
+	$KERNEL \
+	$MODULES_FS \
+    $ROOTFS \
+	$OPK_FILE \
+	-no-progress -noappend -comp gzip -all-root
+
+echo
+echo "=========================="
+echo
+echo "updater OPK:       $OPK_FILE"
+echo
+
+mv $OPK_FILE ../output/
+echo
+echo "=========================="
+echo
+echo "moved OPK to output folder"
+echo
+
+rm updateplayv2/default.gcw0.desktop updateplayv2/date.txt updateplayv2/version.txt updateplayv2/*.sha1
