7cad12dfe7e4ce6e5c790cab4ad59fb02e73e181
Ninoh-FOX <ninohfox@gmail.com>
2021-03-09 07:45:21 +0100

    add alsa set default volume level

--- board/opendingux/pg2v2/target_skeleton/etc/init.d/S90volume.sh
+++ board/opendingux/pg2v2/target_skeleton/etc/init.d/S90volume.sh
@@ -18,14 +18,20 @@ case "$1" in
 		if [ -f $VOLUME_STATEFILE1 ]; then
 			echo "Loading sound volume..."
 			/usr/bin/alsa-setvolume $VOLUME_DEVICE1 $VOLUME_CONTROL1 `cat $VOLUME_STATEFILE1`
+			else
+			/usr/bin/alsa-setvolume $VOLUME_DEVICE1 $VOLUME_CONTROL1 31
 		fi
 		if [ -f $VOLUME_STATEFILE2 ]; then
 			echo "Loading sound volume..."
 			/usr/bin/alsa-setvolume $VOLUME_DEVICE2 $VOLUME_CONTROL2 `cat $VOLUME_STATEFILE2`
+			else
+			/usr/bin/alsa-setvolume $VOLUME_DEVICE2 $VOLUME_CONTROL2 25
 		fi
 		if [ -f $VOLUME_STATEFILE3 ]; then
 			echo "Loading sound volume..."
 			/usr/bin/alsa-setvolume $VOLUME_DEVICE3 $VOLUME_CONTROL3 `cat $VOLUME_STATEFILE3`
+			else
+			/usr/bin/alsa-setvolume $VOLUME_DEVICE3 $VOLUME_CONTROL3 255
 		fi
 		;;
 	stop)
