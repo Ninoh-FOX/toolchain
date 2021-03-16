37f30882709db6ee050373dbf439cf5a497c1e8c
Ninoh-FOX <ninohfox@gmail.com>
2021-03-16 09:19:29 +0100

    return to PCM volume control

--- board/opendingux/rg280m/target_skeleton/etc/init.d/S90volume.sh
+++ board/opendingux/rg280m/target_skeleton/etc/init.d/S90volume.sh
@@ -9,9 +9,6 @@ VOLUME_CONTROL1=PCM
 VOLUME_STATEFILE2=/usr/local/etc/volume2.state
 VOLUME_DEVICE2=default
 VOLUME_CONTROL2=Headphone
-VOLUME_STATEFILE3=/usr/local/etc/volume3.state
-VOLUME_DEVICE3=default
-VOLUME_CONTROL3=Master
 
 case "$1" in
 	start)
@@ -27,18 +24,11 @@ case "$1" in
 			else
 			/usr/bin/alsa-setvolume $VOLUME_DEVICE2 $VOLUME_CONTROL2 25
 		fi
-		if [ -f $VOLUME_STATEFILE3 ]; then
-			echo "Loading sound volume..."
-			/usr/bin/alsa-setvolume $VOLUME_DEVICE3 $VOLUME_CONTROL3 `cat $VOLUME_STATEFILE3`
-			else
-			/usr/bin/alsa-setvolume $VOLUME_DEVICE3 $VOLUME_CONTROL3 255
-		fi
 		;;
 	stop)
 		echo "Storing sound volume..."
 		/usr/bin/alsa-getvolume $VOLUME_DEVICE1 $VOLUME_CONTROL1 > $VOLUME_STATEFILE1
 		/usr/bin/alsa-getvolume $VOLUME_DEVICE2 $VOLUME_CONTROL2 > $VOLUME_STATEFILE2
-		/usr/bin/alsa-getvolume $VOLUME_DEVICE3 $VOLUME_CONTROL3 > $VOLUME_STATEFILE3
 		;;
 	*)
 		echo "Usage: $0 {start|stop}"
