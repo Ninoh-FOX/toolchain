ac0f9f0c367ad1008839f50cfa5b6a3fa1e48f8a
Ninoh-FOX <ninohfox@gmail.com>
2021-05-24 22:11:22 +0200

    update liblightning package

--- package/lightning/lightning.mk
+++ package/lightning/lightning.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-LIGHTNING_VERSION = 2.0.4
+LIGHTNING_VERSION = 2.1.3
 LIGHTNING_SITE = http://ftp.gnu.org/gnu/lightning/
 LIGHTNING_LICENSE = GPLv3
 LIGHTNING_LICENSE_FILES = COPYING
 LIGHTNING_INSTALL_STAGING = YES
+LIGHTNING_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_BINUTILS),y)
-LIGHTNING_DEPENDENCIES += binutils
+LIGHTNING_DEPENDENCIES += binutils zlib
 LIGHTNING_CONF_OPT += --enable-disassembler
 endif
 
