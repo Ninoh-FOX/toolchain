4129b467727739a639ac32b3bf78d8dcf137bed1
Ninoh-FOX <ninohfox@gmail.com>
2021-07-05 08:11:00 +0200

    Update python3.mk

--- package/python3/python3.mk
+++ package/python3/python3.mk
@@ -39,7 +39,7 @@ HOST_PYTHON3_CONF_ENV += \
 
 PYTHON3_DEPENDENCIES  = host-python3 libffi
 
-HOST_PYTHON3_DEPENDENCIES = host-expat host-zlib
+HOST_PYTHON3_DEPENDENCIES = host-expat host-zlib host-openssl
 
 PYTHON3_INSTALL_STAGING = YES
 
